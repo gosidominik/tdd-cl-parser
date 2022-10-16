e3c525a66c399d7e281bbb25f671445d27405f16
gosidominik <gosidominik@gmail.com>
2022-10-16 20:52:35 +0200

    package and config files

---
+++ dist/calc.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Calc = /** @class */ (function () {
+    function Calc() {
+    }
+    Calc.prototype.add = function (a, b) {
+        return a + b;
+    };
+    return Calc;
+}());
+exports.default = new Calc();
+//# sourceMappingURL=calc.js.map
